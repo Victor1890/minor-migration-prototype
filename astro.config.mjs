a6bcc7660492580a8f8950a6aeaa1fb3afe49407
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2025-09-14 15:17:23 -0400

    refactor: set output option to 'static' in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   trailingSlash: 'never',
   site: 'https://permisosalidamenor.migracion.gob.do',
   vite: {
